99fe11e2d835fa001fbc32a872e8d3da3cc3b317
Ian <andreykochnov@yandex.ru>
2019-08-29 23:01:43 +0300

    Second working version: delete extra func

--- source/format.js
+++ source/format.js
@@ -1,6 +1,3 @@
-const getFormattedString = (str, len, placeholder) => {
-    return placeholder.repeat(len - str.length) + str;
-}
 const format = (args, cols) => {
     let lens = new Array(cols).fill(0);
     let res = "";
@@ -10,7 +7,8 @@ const format = (args, cols) => {
     console.log(lens);
     for(let i = 0; i < args.length; ++i) {
         if(i%cols != 0) res += ' ';
-        res += getFormattedString(String(args[i]), lens[i%cols], " ");
+        //res += getFormattedString(String(args[i]), lens[i%cols], " ");
+        res +=' '.repeat(lens[i%cols] - String(args[i]).length) + args[i];
         if(i%cols == cols-1 && i != args.length-1) res += '\n';
     }
     return res;
