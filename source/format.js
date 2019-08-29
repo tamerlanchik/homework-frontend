a9198c65e9e1b6b9e0bd34b7048986c33e0e4d98
Ian <andreykochnov@yandex.ru>
2019-08-29 22:58:34 +0300

    First forking version

---
+++ source/format.js
@@ -0,0 +1,21 @@
+const getFormattedString = (str, len, placeholder) => {
+    return placeholder.repeat(len - str.length) + str;
+}
+const format = (args, cols) => {
+    let lens = new Array(cols).fill(0);
+    let res = "";
+    for(let i = 0; i < args.length; ++i){
+        lens[i % cols] = Math.max(lens[i % cols], String(args[i]).length);
+    }
+    console.log(lens);
+    for(let i = 0; i < args.length; ++i) {
+        if(i%cols != 0) res += ' ';
+        res += getFormattedString(String(args[i]), lens[i%cols], " ");
+        if(i%cols == cols-1 && i != args.length-1) res += '\n';
+    }
+    return res;
+}
+args =[ 0, 1, 2, 10, 100, 1000, 10000 ];
+cols = 1;
+
+console.log(format(args, cols));
