2b3a04402add0b9897c3d70dd2737f1108af8474
Ian <andreykochnov@yandex.ru>
2019-08-30 17:39:23 +0300

    Комментарии

--- source/format.js
+++ source/format.js
@@ -1,13 +1,16 @@
 const format = (args, cols) => {
     if(cols < 0) return '';
+
     args = args.map(currVal => String(currVal));
     let lens = new Array(cols).fill(0);
+    //  Counting width of the columns
     for(let i = 0; i < args.length; ++i){
         lens[i % cols] = Math.max(lens[i % cols], args[i].length);
     }
 
     let table = [];
     for(let i = 0; i < args.length; i += cols) {
+        //  Generating rows
         table.push(
             args.slice(i, i+cols)
             .map(   (currVal, j) => ' '.repeat(lens[j] - currVal.length) + currVal  )
